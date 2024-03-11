f577f5abd7815c790b2ae53f477ac6a9ea46ccbc
Rubel Mahmud <vxlrubel@gmail.com>
2024-03-11 23:55:18 +0600

    initiate file

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+;(function($){
+    const doc = $(document);
+    class Quicky{
+        constructor(){
+
+        }
+    }
+
+    // initiate the programm.
+    doc.ready(()=>{ new Quicky() });
+})(jQuery);
