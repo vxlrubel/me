92dc1fc625473f4c2c03e55664d22be0b2b45b3b
Rubel Mahmud <vxlrubel@gmail.com>
2024-03-12 23:01:52 +0600

    create header and footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,40 @@
     const doc = $(document);
     class Quicky{
         constructor(){
+            this.menuToggle();
+            this.addActiveClass();
+        }
 
+        menuToggle(){
+            const menu = $('.menu');
+            $('#menu-toggle-button').on('click', function(e){
+                e.preventDefault();
+                let icon = $(this).children('i');
+                if( icon.hasClass('fa-bars-staggered') ){
+                    icon.removeClass('fa-bars-staggered').addClass('fa-xmark');
+                    menu.stop().slideDown();
+                }else{
+                    icon.removeClass('fa-xmark').addClass('fa-bars-staggered');
+                    menu.stop().slideUp();
+                }
+            })
+        }
+
+        addActiveClass(){
+            // get location path
+            var currentUrl = window.location.pathname;
+            $('.menu li a').each(function() {
+                let _this    = $(this);
+                let linkHref = _this.attr('href');
+
+                if ( linkHref.startsWith('.') ){
+                    linkHref = linkHref.substring(1);
+                }
+
+                if ( linkHref === currentUrl ) {
+                    _this.parent('li').addClass('active');
+                }
+            });
         }
     }
 
